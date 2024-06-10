5e3bf0e6843ec7cdccb7988c62deacc54e7fd103
Chris-AM <c.aranguizm@outlook.com>
2024-06-09 23:16:08 -0400

    :construction: starts CI/CD. MISSING => prod && .github/

--- src/app/config/app.environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  base_url: 'http://localhost:3000',
-  icons: '/weather-icons',
-  weatherAPI: 'https://api.openweathermap.org/data/2.5/weather?q=',
-  appId:`&appid=${process.env["OPEN_WEATHER_API_KEY"]}&units=metric`,
-};
