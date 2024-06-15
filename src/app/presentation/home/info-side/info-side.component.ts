03d119338222810c00afaeb679d21992363e4a5d
Chris-AM <c.aranguizm@outlook.com>
2024-06-15 02:54:01 -0400

    :construction: can search for city name. MISSING => pass data to weather side component

--- src/app/presentation/home/info-side/info-side.component.ts
+++ src/app/presentation/home/info-side/info-side.component.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { WeatherModel } from '../../../domain/models/weather.model';
 import { SearchContainerComponent } from './search-container/search-container.component';
 
 @Component({
   selector: 'app-info-side',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [SearchContainerComponent],
   templateUrl: './info-side.component.html',
-  styleUrl: './info-side.component.scss'
+  styleUrl: './info-side.component.scss',
 })
 export class InfoSideComponent {
   @Input() public weather!: WeatherModel;
+  @Output() public searchCity = new EventEmitter<WeatherModel>();
+
+  public handleSearchCity(searchTerm: WeatherModel): void {
+    this.weather = searchTerm;
+  }
 }
--- src/app/presentation/home/info-side/search-container/search-container.component.ts
+++ src/app/presentation/home/info-side/search-container/search-container.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IWeather } from '../../../../domain/interfaces/open-weather.interface';
+import { HomeService } from '../../../../use_cases/home.service';
+import { WeatherModel } from '../../../../domain/models/weather.model';
 
 @Component({
   selector: 'app-search-container',
   standalone: true,
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [ReactiveFormsModule],
   templateUrl: './search-container.component.html',
-  styleUrl: './search-container.component.scss'
+  styleUrl: './search-container.component.scss',
 })
 export class SearchContainerComponent {
+  @Input() public foundCity!: WeatherModel;
+  @Output() public searchCity = new EventEmitter<WeatherModel>();
 
+  public searchForm = new FormGroup({
+    searchTerm: new FormControl('') as FormControl<string>,
+  });
+
+  private readonly service: HomeService = inject(HomeService);
+
+  public filterCity(filer: string | undefined): void {
+    if (filer) {
+      this.service.getWeatherByCity(filer).subscribe({
+        next: (city) => {
+          this.foundCity = city;
+          this.searchCity.emit(city);
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
+    }
+  }
 }
